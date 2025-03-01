bea4cda30376aa53ac4b4abe18c3ed145f758807
ishaan766 <ishaan766@gmail.com>
2025-03-02 00:25:10 +0530

    # Elastic Instrumentation

--- sample_app/wsgi.py
+++ sample_app/wsgi.py
@@ -8,8 +8,14 @@ https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
 """
 
 import os
+# import logging
 
 from django.core.wsgi import get_wsgi_application
+
+# If using ELASTIC_APM_LOG_FILE to check agent debug logs, 
+# The following may need to be uncommented to see the logs.
+
+# logging.basicConfig()
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'sample_app.settings')
 
