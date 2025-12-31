bb4daa6998273014ae950ac11352fae8155bfc41
abhinavcubeapm <abhinav@cubeapm.com>
2025-12-31 15:15:32 +0530

    Update wsgi.py adding processId

--- sample_app/wsgi.py
+++ sample_app/wsgi.py
@@ -48,6 +48,7 @@ RequestsInstrumentor().instrument()
 def init_tracing():
     resource = resources.Resource.create({
         resources.HOST_NAME: gethostname() or 'UNSET',
+        resources.PROCESS_PID : os.getpid(),
     })
 
     if os.getenv('OTEL_LOG_LEVEL', '') == 'debug':
